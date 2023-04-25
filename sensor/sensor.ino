0388b9f07865ffb60a624303f76005765ff715ee
Massimo Giardina <massimogg@protonmail.com>
2023-04-25 17:09:27 +0200

    Pendulum werkt, PID werkt

--- sensor/sensor.ino
+++ sensor/sensor.ino
@@ -21,24 +21,35 @@ TODO:
 // Sensor
 #include <Arduino_LSM9DS1.h>
 
-int PID(double curVal);
-void setMotorX(int16_t dc);
-void setMotorY(int16_t dc);
-float getPitch(float ax, float ay, float az);
-float getRoll();
-
 #define PWM_FREQ 0x03
-
 #define PIN_PWM_X 9
 #define PIN_X_LEFT 7
 #define PIN_X_RIGHT 6
 
-float setPoint = 0;
+#define DEBUG 0
+
+double PID(double curVal);
+void setMotorX(int16_t dc);
+void setMotorY(int16_t dc);
+float getGyro();
+float getMagnet();
+float getPitch(float ax, float ay, float az);
+float getRoll();
+float getAvgRoll();
+
+// Uitmiddelen van sensor
+#define AANTAL_GEMIDDELD 2
+float metingen[AANTAL_GEMIDDELD];
+
 unsigned long prevTime = 0;
 unsigned long curTime = 0;
 
 /// @brief PID values
-const float kp=0.1, ki=0.0, kd=0.0;
+/*
+Goeie waardes
+const float kp = 3, ki = 0.00, kd = 10;
+*/
+const float kp = 10, ki = 0.00, kd = 5;
 
 void setup() {
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -82,9 +93,10 @@ void setup() {
    * Calibreer rustpunt
    */
   // Bepaal hoek
+  /*
   setPoint = getRoll();
   Serial.print("New Setpoint is: ");
-  Serial.println(setPoint);
+  Serial.println(setPoint);*/
 }
 
 /**
@@ -92,29 +104,83 @@ void setup() {
  */
 void regelDit() {
   // Huidige hoek gemeten door sensor
-  float huidigeHoek;
-  int output;
+  float currentAngle, currentAcceleration;
+  double output;
+  const int StootHoek = 5;
 
   // Grootste hoek gemeten tot nu toe
   static float grootsteHoek;
   // De huidige PWM die aangelegd wordt aan de motor
   static int huidigePWM;
   static int stoot;
-
-  huidigeHoek = getRoll();
-  // Als er geen hoek is, skip deze iteratie
-  if (huidigeHoek == 1000)
-    return;
-  Serial.print(huidigeHoek);
+  static bool stootGegeven;
 
   /**
-   * Update telkens de grootste huidige hoek
+   * Meting hoek - Roll
+   * Links  = -90
+   * Rechts = +90
    */
-  if (abs(huidigeHoek) > abs(grootsteHoek)) {
+   /*
+  huidigeHoek = getAvgRoll();
+  // Als er geen hoek is, skip deze iteratie
+  if (huidigeHoek == 999)
+    return;*/
+  currentAcceleration = getGyro() - 4.5;
+  //currentAngle = getAvgRoll();
+
+  float mx, my, mz;
+  if (!IMU.magneticFieldAvailable())
+    return;
+
+/* TODO: Zoeken wanneer die omgekeerd is, tusesn twee hoeken minteken omdraaien*/
+  IMU.readMagneticField(mx, my, mz);
+  Serial.print(mx);
+  Serial.print(" \t");
+  Serial.print(my);
+  Serial.print(" \t");
+  Serial.print(mz);
+  Serial.print(" \t");
+
+  /*
+  if (currentAngle == 999)
+    return;*/
+
+  /**
+   * Update telkens de grootste huidige hoek EN als de hoek boven de RESETWAARDE is
+   */
+  Serial.print(" \tCurrentAngle: ");
+  Serial.print(currentAngle);
+  
+  Serial.print(" \tCurrentAcceleration: ");
+  Serial.print(currentAcceleration);
+
+  output = PID(currentAcceleration);
+  Serial.print(" \tPID output: ");
+  Serial.print(output);
+
+  /**
+   * Factor aanpassing
+   */
+  // Opslingeren
+  huidigePWM = -output;
+
+  Serial.print(" \tHuidigePWM: ");
+  Serial.print(huidigePWM);
+
+  Serial.println();
+
+  setMotorX(huidigePWM);
+   /*
+  if (abs(huidigeHoek) > abs(grootsteHoek) && abs(huidigeHoek) > StootHoek) {
+    stootGegeven = false;
     grootsteHoek = huidigeHoek;
 
+    Serial.print(" \tHuidigeHoek: ");
+    Serial.print(huidigeHoek);
+
     // Bereken aan te leggen versnelling bij grootste hoek
-    output = PID(huidigeHoek);
+    output = pid.Run(huidigeHoek);
+    //output = PID(huidigeHoek);
     Serial.print(" \tPID output: ");
     Serial.print(output);
 
@@ -122,83 +188,53 @@ void regelDit() {
     // Verschil bepaalt in te stellen PWM?
     Serial.print(" \tHuidigePWM: ");
     Serial.print(huidigePWM);
-    huidigePWM += output; // Tel op?
+    //huidigePWM = output * 5;
+    huidigePWM += output;  // Tel op?
+
+    // Begrens PWM tot max
+    if (huidigePWM > 255)
+      huidigePWM = 255;
+    else if (huidigePWM < -255)
+      huidigePWM = -255;
+
     Serial.print(" \tNieuwe PWM: ");
     Serial.print(huidigePWM);
-  }
+    Serial.println();
+  }*/
 
   /*
     Reset als tussen [-10, 10] graden
     MAAR HIER GEVEN WE OOK DE STOOT!
+    BUG: Doe dit slechts 1 keer tot reset!!
   */
-  if (abs(huidigeHoek) < 10) {
-    grootsteHoek = 0;
+  /*
+  if (abs(huidigeHoek) < StootHoek && !stootGegeven) {
     setMotorX(huidigePWM);
-  }
+    Serial.print("Stoot");
+    Serial.println(huidigePWM);
+
+    grootsteHoek = 0;
+    stootGegeven = true;
+  }*/
 }
 
-#define AANTAL_GEMIDDELD 15
-float metingen[AANTAL_GEMIDDELD];
-
 void loop() {
-  unsigned int now = 0, prev = 0;
-
-  now = millis();
-
-  /**
-   * Meting hoek - Roll
-   * Links  = -90
-   * Rechts = +90
-   */
-
   /**
    * Regel systeem aan de hand van de uitgelezen waardes
    */
-  // regelDit();
-  setMotorX(100);
+  regelDit();
 
-  float gemiddeldeHoek = 0;
-
-  static int index;
-  metingen[index] = getRoll();
-  index++;
-  if (index == AANTAL_GEMIDDELD)
-    index = 0;
-
-  // Bereken gemiddelde
-  for (int i = 0; i < AANTAL_GEMIDDELD; i++) {
-    gemiddeldeHoek += metingen[i];
-  }
-
-  gemiddeldeHoek /= AANTAL_GEMIDDELD;
-  Serial.print(90);
-  Serial.print(" ");
-  Serial.print(-90);
-  Serial.print(" ");
-  Serial.print(gemiddeldeHoek);
-
-  /**
-   * Uitmiddelen
-   */
-
-
-  /**
-   * 
-   */
-  delay(2);  // f = 1/0.002
-
-  Serial.println();
-  prev = now;
+  delay(1);  // f = 1/0.002 = 500Hz
 }
 
-int PID(double curVal) {
+double PID(double curVal) {
   static double priError;
   static double toError;
 
   /**
    * Set-waarde om naar te streven voor de PID regelaar
    */
-  int setP = 8;
+  int setP = 0;
 
   double error = setP - curVal;
 
@@ -223,8 +259,7 @@ int PID(double curVal) {
  * @param dc Duty cycle van de motor, negatief = anti-clockwise, positief = clockwise
  */
 void setMotorX(int16_t dc) {
-
-  int MAX = 200;
+  int MAX = 255;
   if (dc < -MAX)
     dc = -MAX;
   if (dc > MAX)
@@ -250,15 +285,72 @@ float getPitch(float ax, float ay, float az) {
   return pitch;
 }
 
+float getMagnet() {
+  float mx, my, mz;
+
+  if (!IMU.magneticFieldAvailable())
+    return 999;
+  
+  IMU.readMagneticField(mx, my, mz);
+  Serial.print((int)mx);
+  Serial.print(" \t");
+  Serial.print((int)my);
+  Serial.print(" \t");
+  Serial.print((int)mz);
+  Serial.print(" \t");
+}
+
+float getGyro() {
+  float ax, ay, az;
+
+  if (!IMU.gyroscopeAvailable())
+    return 999;
+  
+  IMU.readGyroscope(ax, ay, az);
+  return az;
+}
+
 float getRoll() {
   float ax, ay, az;
 
   if (!IMU.accelerationAvailable())
-    return 1000;  // fout
+    return 999;  // fout
 
   IMU.readAcceleration(ax, ay, az);
 
   float roll = atan(ay / sqrt(ax * ax + az * az));
   roll *= 180.0 / PI;
   return roll;
+}
+
+/**
+ * Berekent de gemiddelde hoek
+ */
+float getAvgRoll() {
+  float gemiddeldeHoek = 0;
+
+  static int index;
+  metingen[index] = getRoll();
+  if (metingen[index] == 999) 
+    return 999;
+  
+  index++;
+  if (index == AANTAL_GEMIDDELD)
+    index = 0;
+
+  // Bereken gemiddelde
+  for (int i = 0; i < AANTAL_GEMIDDELD; i++) {
+    gemiddeldeHoek += metingen[i];
+  }
+
+  gemiddeldeHoek /= AANTAL_GEMIDDELD;
+
+#if DEBUG
+  Serial.print(90);
+  Serial.print(" ");
+  Serial.print(-90);
+  Serial.print(" ");
+  Serial.print(gemiddeldeHoek);
+#endif
+  return gemiddeldeHoek;
 }
